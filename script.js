9030487e12bbfa8dd060690a4a80f3de2e93eff6
Philippe Recto <precto1285@gmal.com>
2019-05-12 22:41:26 -0700

    attempting API call to Mindera server

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-// var target = document.getElementById('target');
 
-// target.innerHTML = "Hello World!";
 
-// var people = [];
+//API CALL
+document.getElementById('getImages').addEventListener('click', getImages);
 
+function getImages() {
+  console.log('hello world!');
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'http://127.0.0.1:3000/cards?_start=1&_end=10', true);
+
+  xhr.onload = function () {
+    if (this.status = 200) {
+      const response = this.responseText;
+      console.log(response);
+    }
+  }
+
+}
+
+//CAROUSEL LOAD
+
+
+// CAROUSEL
 carousel = (function () {
+  // Variables
   var box = document.querySelector('.carouselbox');
   var next = box.querySelector('.next');
   var prev = box.querySelector('.prev');
@@ -13,6 +33,8 @@ carousel = (function () {
   var amount = items.length;
   var current = items[0];
   box.classList.add('active');
+
+  // Navigation functions for buttons
   function navigate(direction) {
     current.classList.remove('current');
     counter = counter + direction;
@@ -27,11 +49,16 @@ carousel = (function () {
     current = items[counter];
     current.classList.add('current');
   }
+
+  // Event Listener for the next button
   next.addEventListener('click', function (ev) {
     navigate(1);
   });
+
+  // Event Listener for the previous button
   prev.addEventListener('click', function (ev) {
     navigate(-1);
   });
+
   navigate(0);
 })();
